e0670a1939435c8b4547d5246c729afc14130c06
Beatriz Felix <azz.felix7@gmail.com>
2020-05-03 06:25:35 -0300

    chore(backend): cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 
+import cors from 'cors'
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import routes from './routes';
@@ -10,6 +11,7 @@ import './database';
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.director));
 app.use(routes);
