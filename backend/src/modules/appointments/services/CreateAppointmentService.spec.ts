ee434ebe9f946280af5f2e3a79004f02c62f3df4
Beatriz Felix <azz.felix7@gmail.com>
2020-06-04 17:37:20 -0300

    test(backend): update create appointment

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -3,13 +3,16 @@ import AppError from '@shared/errors/AppError';
 import FakeAppointmentRepository from '@modules/appointments/infra/repositories/fakes/FakeAppointmentRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
-describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', async () => {
-    const fakeAppointmentRepository = new FakeAppointmentRepository();
-    const createAppointment = new CreateAppointmentService(
-      fakeAppointmentRepository,
-    );
+let fakeAppointmentRepository: FakeAppointmentRepository;
+let createAppointment: CreateAppointmentService;
 
+describe('CreateAppointment', () => {
+  beforeEach(() => {
+    fakeAppointmentRepository = new FakeAppointmentRepository();
+    createAppointment = new CreateAppointmentService(fakeAppointmentRepository);
+  });
+
+  it('should be able to create a new appointment', async () => {
     const provider_id = uuid();
 
     const appointment = await createAppointment.execute({
@@ -22,15 +25,10 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    const fakeAppointmentRepository = new FakeAppointmentRepository();
-    const createAppointment = new CreateAppointmentService(
-      fakeAppointmentRepository,
-    );
-
     const provider_id = uuid();
     const date = new Date(2020, 5, 13, 11);
 
-    const appointment = await createAppointment.execute({
+    await createAppointment.execute({
       date,
       provider_id,
     });
